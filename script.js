a680f73ae3721070a7297fc7bc4081a7e0221725
bjh26 <bjhuang@umass.edu>
2025-04-12 16:16:40 -0400

    Fixes #5, Fixes #10. Colored states based on average price of desired year

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ let ANNUAL_STATE_DATA = {};
 let QUARTERLY_STATE_DATA = {};
 
 // Dictionary for converting state names
-const STATE_NAME_DICT = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"};
+// Added DC since it wasn't previously added
+const STATE_NAME_DICT = {"DC":"District of Columbia", "AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"};
 
 // Initialize the website
 start();
@@ -21,32 +22,9 @@ async function start() {
 
     // Load in our json data
     await loadData();
-
-    // Create an svg to hold the map
-    let svg = d3.select(".map-div").append("svg")  
-                                   .attr("id", "map-svg")
-                                   .attr("height", MAP_HEIGHT)
-                                   .attr("width", MAP_WIDTH);
-
-    // Convert spherical coordinates to 2D cooordinates 
-    let projection = d3.geoAlbersUsa()
-        .translate([MAP_WIDTH / 2, MAP_HEIGHT / 2]) // this centers the map in our SVG element
-        .scale([1300]); // this specifies how much to zoom
-    
-    // Convert the projected lat/lon coordinates into an SVG path string
-    let path = d3.geoPath()
-                 .projection(projection);
-    
-    // Create states
-    let states = svg.selectAll(".state")
-    states.data(PATH_DATA.features).enter()
-                                   .append("path")
-                                   .attr("class", "state")
-                                   .attr("d", path)
-                                   .attr("id", d => d.properties.name)
-                                   .on("click", stateCard);
-
-}
+    generateMap()
+   
+}   
 
 // Helper for loading in json and initializing our dictionaries
 async function loadData() {
@@ -70,6 +48,47 @@ async function loadData() {
     // TODO: Add city data parsing here
 }
 
+function generateMap(){
+    // remove previous on each refresh
+    d3.select('svg').remove();
+
+    // Create an svg to hold the map
+        let svg = d3.select(".map-div").append("svg")  
+                                       .attr("id", "map-svg")
+                                       .attr("height", MAP_HEIGHT)
+                                       .attr("width", MAP_WIDTH);
+
+     // Convert spherical coordinates to 2D cooordinates 
+     let projection = d3.geoAlbersUsa()
+     .translate([MAP_WIDTH / 2, MAP_HEIGHT / 2]) // this centers the map in our SVG element
+     .scale([1300]); // this specifies how much to zoom
+
+    // Convert the projected lat/lon coordinates into an SVG path string
+    let path = d3.geoPath()
+              .projection(projection);
+
+    const yearStatePrice = Object.values(ANNUAL_STATE_DATA).map((stateData) => stateData[0]['Average Price'])
+    console.log(yearStatePrice)
+
+    const colorScale = d3.scaleLinear() //d3.select("#metric").node().value)
+                      .domain([d3.min(yearStatePrice), d3.max(yearStatePrice)])
+                      .range(["#b7efc5", "#1a7431", "#10451d"]); 
+
+
+    const wantedYear = d3.min([9, parseInt(d3.select('#year').node().value.substring(2))])
+
+    // Create states
+    let states = svg.selectAll(".state")
+    states.data(PATH_DATA.features).enter()
+                                .append("path")
+                                .attr("class", "state")
+                                .attr("d", path)
+                                .style('fill', (d) => colorScale(((ANNUAL_STATE_DATA[d.properties.name])[wantedYear])['Average Price']))
+                                .attr("id", d => d.properties.name)
+                                .on('mouseover', (event, _d) => d3.select(event.currentTarget).style("fill", "#68b0ab"))
+                                .on("mouseout", (event, d) => d3.select(event.currentTarget).style("fill", colorScale(((ANNUAL_STATE_DATA[d.properties.name])[wantedYear])['Average Price'])))
+                                .on("click", stateCard);
+}
 
 // PLACEHOLDER: Currently helps show functionality of code
 function stateCard(event, d) { 
@@ -87,3 +106,11 @@ function stateCard(event, d) {
     console.log(curState + "'s avg house price over the years:");
     ANNUAL_STATE_DATA[curState].forEach(instance => console.log(instance['Year'] + ': ' + instance['Average Price']));
 }
+
+// slider
+const slider = d3.select('#year');
+const yearBox = d3.select('#yearBox');
+slider.on('input', function(){
+    yearBox.property('value', this.value);
+    generateMap();
+});
